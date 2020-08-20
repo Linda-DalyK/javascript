33a15ee5c638636bcb8ccf8840be1a7eeda6e181
Linda-K-daly <68587994+Linda-K-daly@users.noreply.github.com>
2020-08-20 17:00:00 +0200

    2008

---
+++ Jour_3/isPalindrome.js
@@ -0,0 +1,38 @@
+// créer une fonction qui prend une string et qui retourne TRUE si la string est un palindrome ou FALSE
+// si ce n'est pas un palindrome
+// si la phrase à l'endroit === la phrase à l'envers ===> TRUE
+// il faut que la boucle lise chaque caractère l'un après l'autre à l'endroit et à l'envers
+
+// 1ère boucle lit la string à l'endroit return: les caractères un à un à l'endroit
+// 2è boucle lit la string à l'envers return: les caractères un à un à l'envers
+
+// si return1 === return2 ===> TRUE
+// sinon FALSE 
+
+
+
+//  function isPalindrome(str) {
+
+var s = 'never odd or even';
+var rev = s.split('');
+// console.log(rev);
+
+for (var i = str.length; i >= 0; i--); {
+    result = (rev)
+
+}
+return result
+
+}
+console.log(result)
+
+
+
+    // for (var i = 1; i < str.length ; i++); {
+    //   result = 
+
+//     }
+
+// }
+
+    // console.log(str.split(''))
--- Jour_4/doop.js
+++ Jour_4/doop.js
@@ -3,30 +3,39 @@
 
 function doop(param1, param2, param3) {
 
+  
   var param1 = parsInt(process.argv[2]);
   var param2 = parsInt(process.argv[3]);
   var param3 = parsInt(process.argv[4]);
 
 
-  //var grade = null;
-  console.log(param2);
-  switch (grade) {
+  
+  console.log();
+  switch (param2) {
     case '+':
-      console.log(param1 + param3);    // Addition
-      break;
+      console.log(doop(param1,param2,param3));    // Addition
+      
     case '-':
-      console.log(param1 - param3);  // Soustraction
-      break;
+      console.log(doop(param1,param2,param3));  // Soustraction
+     
     case '*':
-      console.log(number1 * number3);  // Multplication
-      break;
+      console.log(doop(param1,param2,param3));  // Multplication
+     
     case '/':
-      console.log(number1 / number3);  // Division
+      console.log(doop(param1,param2,param3));  // Division
+
     case (doop.length > 3):
       console.log("error");  // Si le nombre de paramètres est>3= error
-      break;
+      
     default:
       console.log("not rated");  // Instructions à exécuter lorsqu'aucune des valeurs ne correspond
 
   }
+  return 
 }
+
+console.log(doop())
+
+//var myArgs = process.argv.slice(2);
+//console.log('myArgs: ', myArgs);
+
